c9d6ae987d7c63585ca5b9e44faa0fc84d6170d9
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-21 11:51:59 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! come va?");
+
+var risposta = Console.ReadLine();
+Console.WriteLine("La risposta è" + risposta);
